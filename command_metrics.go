9d47ab35dd80f20db259b3508b1605bd74a93270
tom--bo <otsuka.t.2013@gmail.com>
2016-10-02 08:20:18 +0000

    Fix

--- command_metrics.go
+++ command_metrics.go
@@ -1,15 +1,11 @@
 package command_metrics
 
-import (
-	"time"
-)
-
 // Enable is true when logging is on
 var Enable = true
 
 // WrapCommand simply wrap originl exec.command
 func WrapCommand(name string, command commandInterface) *Command {
-	metrics = newMetrics(name)
-	proxy = newCommand(command, metrics)
+	metrics := newMetrics(name)
+	proxy := newCommand(command, metrics)
 	return proxy
 }
