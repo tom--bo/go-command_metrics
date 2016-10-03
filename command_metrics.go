af18f7b1b6fc252c744cb05124d4f96205221290
tom--bo <otsuka.t.2013@gmail.com>
2016-10-03 13:17:06 +0900

    コマンドのプロキシは不要。print,flushコマンドも削除

--- command_metrics.go
+++ command_metrics.go
@@ -1,12 +1,7 @@
 package command_metrics
 
-import (
-	"time"
-)
-
 var Enable = true
 
-var proxyRegistry = make(map[commandInterface](*Command))
 var metricsRegistry = make(map[string](*Metrics))
 
 func WrapCommand(name string, command commandInterface) *Command {
@@ -15,34 +10,8 @@ func WrapCommand(name string, command commandInterface) *Command {
 		metrics = newMetrics(name)
 		metricsRegistry[name] = metrics
 	}
-	proxy := proxyRegistry[command]
-	if proxy == nil {
-		proxy = newCommand(command, metrics)
-		proxyRegistry[command] = proxy
-	}
+    proxy := newCommand(command, metrics)
 	return proxy
 }
 
-func Flush() {
-	for _, metrics := range metricsRegistry {
-		metrics.printMetrics(-1)
-	}
-}
 
-func Print(duration int) {
-	if duration <= 0 {
-		return
-	}
-	timeDuration := time.Duration(duration)
-	go func() {
-		time.Sleep(timeDuration * time.Second)
-		for {
-			startTime := time.Now()
-			//for _, metrics := range metricsRegistry {
-			//	metrics.printMetrics(duration)
-			//}
-			elapsedTime := time.Now().Sub(startTime)
-			time.Sleep(timeDuration*time.Second - elapsedTime*time.Second)
-		}
-	}()
-}
